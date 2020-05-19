55624c899e70972e99981a17ce23bbe5a0cb20de
exlineo <contact@exlineo.com>
2020-05-19 20:14:50 +0200

    Fonctions de base fonctionnelles

--- dd5WSServer.js
+++ dd5WSServer.js
@@ -13,29 +13,31 @@ io.on('connection', (socket) => {
     io.emit('connect', socket);
 
     // On indique qu'on est le maître de jeu
-    socket.on('master', (id) => {
-        console.log(id, socket.id);
+    socket.on('master', (msg) => {
+        console.log(msg);
+        master = socket.id;
+        io.emit('masterConnecte', msg);
     });
 
     // Réception d'un message envoyé par le client
-    socket.on('messageEnvoye', (message) => {
-        console.log(message);
-        io.emit('messageRecu', message);
+    socket.on('messageEnvoye', (msg) => {
+        console.log(msg);
+        io.emit('messageRecu', msg);
     });
 
     // Réception d'un message envoyé par le client
-    socket.on('lienEnvoye', (message) => {
-        console.log(message);
-        io.emit('lienRecu', message);
+    socket.on('lienEnvoye', (msg) => {
+        console.log(msg);
+        io.emit('lienRecu', msg);
     })
 
     // Envoie des informations d'un joueur pour le maître de jeu
-    socket.on('persoEnvoi', (message) => {
-        console.log(message);
-        io.emit('persoRecu', message);
+    socket.on('persoEnvoi', (stat) => {
+        console.log(stat);
+        io.emit('persoRecu', stat);
     })
 });
 
-http.listen(6666, function() {
-    console.log('listening on *:6666');
+http.listen(666, function() {
+    console.log('listening on *:666');
 });
